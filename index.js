3438b721e3b93b940616e38ae38d1022b01d44c6
NIhilth <91199065+NIhilth@users.noreply.github.com>
2021-11-13 12:51:49 -0300

    yey

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
 let chat = document.querySelector("#chat");
 let input = document.querySelector("#mensagem");
 let seuNome = document.querySelector('#nome');
